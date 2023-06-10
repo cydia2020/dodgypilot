83e1903d3edd7b589b3749a9a0d03d11d929518a
cydia2020 <12470297+cydia2020@users.noreply.github.com>
2023-06-10 10:30:31 +1000

    also multiply for a_change_cost

--- selfdrive/controls/lib/longitudinal_mpc_lib/long_mpc.py
+++ selfdrive/controls/lib/longitudinal_mpc_lib/long_mpc.py
@@ -285,7 +285,7 @@ class LongitudinalMpc:
     cost_mulitpliers = self.get_cost_multipliers(v_lead0, v_lead1)
     if self.mode == 'acc':
       a_change_cost = A_CHANGE_COST if prev_accel_constraint else 0
-      cost_weights = [X_EGO_OBSTACLE_COST, X_EGO_COST, V_EGO_COST, A_EGO_COST, a_change_cost * cost_mulitpliers[0], J_EGO_COST * cost_mulitpliers[1]]
+      cost_weights = [X_EGO_OBSTACLE_COST, X_EGO_COST, V_EGO_COST, A_EGO_COST, a_change_cost * cost_mulitpliers[0] * cost_mulitpliers[1], J_EGO_COST * cost_mulitpliers[1]]
       constraint_cost_weights = [LIMIT_COST, LIMIT_COST, LIMIT_COST, DANGER_ZONE_COST]
     elif self.mode == 'blended':
       cost_weights = [0., 0.2, 0.25, 1.0, 0.0, 1.0]
