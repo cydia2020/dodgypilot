a0ecb007a0dcafc8ebe9151c968964b30877ed16
cydia2020 <12470297+cydia2020@users.noreply.github.com>
2024-01-30 18:43:48 +1100

    use debug cert for panda

--- release/build_release.sh
+++ release/build_release.sh
@@ -57,7 +57,7 @@ export PYTHONPATH="$BUILD_DIR"
 scons -j$(nproc)
 
 # release panda fw
-CERT=/data/pandaextra/certs/release RELEASE=1 scons -j$(nproc) panda/
+CERT=panda/certs/release RELEASE=0 scons -j$(nproc) panda/
 
 # Ensure no submodules in release
 if test "$(git submodule--helper list | wc -l)" -gt "0"; then
