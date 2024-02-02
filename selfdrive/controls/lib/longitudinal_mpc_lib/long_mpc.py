86f378e5593ef556c878624280d79c28a11b123f
cydia2020 <12470297+cydia2020@users.noreply.github.com>
2024-02-02 13:20:36 +1100

    even smaller

--- selfdrive/controls/lib/longitudinal_mpc_lib/long_mpc.py
+++ selfdrive/controls/lib/longitudinal_mpc_lib/long_mpc.py
@@ -70,9 +70,9 @@ def get_a_change_cost_multiplier(personality=log.LongitudinalPersonality.standar
   if personality==log.LongitudinalPersonality.relaxed:
     return 1.0
   elif personality==log.LongitudinalPersonality.standard:
-    return 0.93
+    return 0.8
   elif personality==log.LongitudinalPersonality.aggressive:
-    return 0.85
+    return 0.6
   else:
     raise NotImplementedError("Longitudinal personality not supported")
 
