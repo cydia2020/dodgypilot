0c66305d118ac87227f65ac0c3cf9e84698f10ee
cydia2020 <12470297+cydia2020@users.noreply.github.com>
2024-02-07 12:45:49 +1100

    don't deadzone when zss

--- selfdrive/car/toyota/interface.py
+++ selfdrive/car/toyota/interface.py
@@ -59,7 +59,7 @@ class CarInterface(CarInterfaceBase):
       ret.mass = 3045. * CV.LB_TO_KG
       # Only give steer angle deadzone to for bad angle sensor prius
       for fw in car_fw:
-        if fw.ecu == "eps" and not fw.fwVersion == b'8965B47060\x00\x00\x00\x00\x00\x00':
+        if fw.ecu == "eps" and not (fw.fwVersion == b'8965B47060\x00\x00\x00\x00\x00\x00' or 0x23 in fingerprint[0]):
           ret.steerActuatorDelay = 0.25
           CarInterfaceBase.configure_torque_tune(candidate, ret.lateralTuning, steering_angle_deadzone_deg=0.2)
 
