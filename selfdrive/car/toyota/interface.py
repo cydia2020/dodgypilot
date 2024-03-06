13f472d1875b966aa27e2005671b43b0027cc81c
Shane Smiskol <shane@smiskol.com>
2024-03-05 22:50:14 -0800

    Toyota Highlander: also check unplugged DSU to enable sng (#31723)
    * also check unplugged DSU
    
    * fix

--- selfdrive/car/toyota/interface.py
+++ selfdrive/car/toyota/interface.py
@@ -60,6 +60,11 @@ class CarInterface(CarInterfaceBase):
     if 0x23 in fingerprint[0]:
       ret.flags |= ToyotaFlags.SECONDARY_STEER_ANGLE.value
 
+    # In TSS2 cars, the camera does long control
+    found_ecus = [fw.ecu for fw in car_fw]
+    ret.enableDsu = len(found_ecus) > 0 and Ecu.dsu not in found_ecus and candidate not in (NO_DSU_CAR | UNSUPPORTED_DSU_CAR) \
+                                        and not (ret.flags & ToyotaFlags.SMART_DSU) and not (ret.flags & ToyotaFlags.DSU_BYPASS)
+
     if candidate == CAR.PRIUS:
       # Only give steer angle deadzone to for bad angle sensor prius
       for fw in car_fw:
@@ -87,7 +92,7 @@ class CarInterface(CarInterfaceBase):
     elif candidate in (CAR.HIGHLANDER, CAR.HIGHLANDER_TSS2):
       # TODO: TSS-P models can do stop and go, but unclear if it requires sDSU or unplugging DSU.
       #  For now, don't list stop and go functionality in the docs
-      stop_and_go = stop_and_go or bool(ret.flags & ToyotaFlags.SMART_DSU.value)
+      stop_and_go = stop_and_go or bool(ret.flags & ToyotaFlags.SMART_DSU.value) or (ret.enableDsu and not docs)
 
     elif candidate in (CAR.AVALON, CAR.AVALON_2019, CAR.AVALON_TSS2):
       # starting from 2019, all Avalon variants have stop and go
@@ -120,11 +125,6 @@ class CarInterface(CarInterfaceBase):
     # No radar dbc for cars without DSU which are not TSS 2.0
     # TODO: make an adas dbc file for dsu-less models
     ret.radarUnavailable = DBC[candidate]['radar'] is None or candidate in (NO_DSU_CAR - TSS2_CAR)
-
-    # In TSS2 cars, the camera does long control
-    found_ecus = [fw.ecu for fw in car_fw]
-    ret.enableDsu = len(found_ecus) > 0 and Ecu.dsu not in found_ecus and candidate not in (NO_DSU_CAR | UNSUPPORTED_DSU_CAR) \
-                                        and not (ret.flags & ToyotaFlags.SMART_DSU) and not (ret.flags & ToyotaFlags.DSU_BYPASS)
 
     # if the smartDSU is detected, openpilot can send ACC_CONTROL and the smartDSU will block it from the DSU or radar.
     # since we don't yet parse radar on TSS2/TSS-P radar-based ACC cars, gate longitudinal behind experimental toggle
