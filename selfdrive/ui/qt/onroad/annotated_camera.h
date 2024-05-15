d5f85f8c32911e1f5e50f38b4b6d40c95093d871
Irene <12470297+cydia2020@users.noreply.github.com>
2024-05-15 16:56:33 +1000

    fix onroad ui

--- selfdrive/ui/qt/onroad/annotated_camera.h
+++ selfdrive/ui/qt/onroad/annotated_camera.h
@@ -28,6 +28,7 @@ private:
   bool is_cruise_set = false;
   bool is_metric = false;
   bool dmActive = false;
+  bool brakeLights = false;
   bool hideBottomIcons = false;
   bool rightHandDM = false;
   float dm_fade_state = 1.0;
@@ -46,7 +47,7 @@ protected:
   void showEvent(QShowEvent *event) override;
   void updateFrameMat() override;
   void drawLaneLines(QPainter &painter, const UIState *s);
-  void drawLead(QPainter &painter, const cereal::RadarState::LeadData::Reader &lead_data, const QPointF &vd);
+  void drawLead(QPainter &painter, const UIScene &scene, const cereal::RadarState::LeadData::Reader &lead_data, const QPointF &vd, float vego);
   void drawHud(QPainter &p);
   void drawDriverState(QPainter &painter, const UIState *s);
   inline QColor redColor(int alpha = 255) { return QColor(201, 34, 49, alpha); }
