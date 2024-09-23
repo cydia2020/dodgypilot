8d81a5b56a2294f3f721bd3a8222290a1292104f
Dean Lee <deanlee3@gmail.com>
2024-09-24 04:48:24 +0800

    ui: refactor hud updating and rendering into HudRenderer class  (#33458)
    refactor hud updating and rendering into DriverMonitorRenderer class

--- selfdrive/ui/qt/onroad.h
+++ selfdrive/ui/qt/onroad.h
@@ -104,13 +104,13 @@ private:
   bool is_cruise_set = false;
   bool is_metric = false;
   bool dmActive = false;
-  bool brakeLights = false;
   bool hideBottomIcons = false;
   bool rightHandDM = false;
   float dm_fade_state = 1.0;
   bool has_us_speed_limit = false;
   bool has_eu_speed_limit = false;
   bool v_ego_cluster_seen = false;
+  bool brake_lights = false;
   int status = STATUS_DISENGAGED;
   std::unique_ptr<PubMaster> pm;
 
