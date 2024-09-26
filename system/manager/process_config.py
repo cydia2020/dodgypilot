5743f60472473a96d920227e7e9d3495e2512b22
ZwX1616 <zwx1616@gmail.com>
2024-09-26 16:40:44 -0700

    exec DM model with gpu (#33609)
    * half
    
    old-commit-hash: 9f72eca003d4637ca7fe847ebaf925c694fc2e84
    
    * optimed
    
    old-commit-hash: 6e36e2a12e09275ec21d1590012a92b05ca52ff5
    
    * thneed
    
    old-commit-hash: 419a06c09c0c767d828bcd1e118bc575898c343a
    
    * exec
    
    old-commit-hash: 0059c27ec11b076a37f65d604ed135ea6541b1a6
    
    * runner
    
    old-commit-hash: 34232ada94450ce541eaef546197fa219810a891
    
    * runs but
    
    old-commit-hash: 3db37c00b6a64908293b4de8d8b56e80308cd8f2
    
    * it is 01
    
    old-commit-hash: a160d81eb1a7e77abbef959b44f602610f68f665
    
    * np
    
    old-commit-hash: c1caff6ba648cc2c0094c71b2ea074f01c3c2dc8
    
    * module url
    
    old-commit-hash: 6f4902c4d384263a53e2c1d14d93b5ff864b6a5f
    
    * new
    
    old-commit-hash: 779ae79b1bc3df6374fb6663ac8592e107a6e504
    
    * ds fast
    
    * is this work
    
    * corcention
    
    * real timing
    
    * no reg
    
    * interim gather
    
    * 0e4a9c7b
    
    * fa69be01, and halve
    
    * list
    
    * cleanup
    
    * slighly faster
    
    * setprotlt
    
    * expected
    
    * replay ref
    
    * more powar
    
    * reluctantly
    
    * bump tg
    
    * 8
    
    * less
    
    * less
    
    * bump tg
    
    * better than exp
    
    * closer
    
    * cc
    
    * see diff
    
    * commits
    
    * was right
    
    * to 32 cast
    
    * remove dlc file
    
    * support both
    
    * dspExecutionTime -> gpuExecutionTime
    
    * ignore
    
    * time ref
    
    * ref commit
    
    * last
    
    ---------
    
    Co-authored-by: Comma Device <device@comma.ai>

--- system/manager/process_config.py
+++ system/manager/process_config.py
@@ -75,7 +75,7 @@ procs = [
   PythonProcess("micd", "system.micd", iscar and soundd_enabled),
   PythonProcess("timed", "system.timed", always_run, enabled=not PC),
 
-  PythonProcess("dmonitoringmodeld", "selfdrive.modeld.dmonitoringmodeld", driverview, enabled=(not PC or WEBCAM)),
+  NativeProcess("dmonitoringmodeld", "selfdrive/modeld", ["./dmonitoringmodeld"], driverview, enabled=(not PC or WEBCAM)),
   NativeProcess("encoderd", "system/loggerd", ["./encoderd"], only_onroad),
   NativeProcess("stream_encoderd", "system/loggerd", ["./encoderd", "--stream"], notcar),
   NativeProcess("loggerd", "system/loggerd", ["./loggerd"], logging),
