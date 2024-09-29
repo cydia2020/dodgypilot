e34981fbcb7bf9a1688fa5788607efd99eb8942d
Irene <12470297+cydia2020@users.noreply.github.com>
2024-09-29 14:56:47 +1000

    reset some previous inconsistencies

--- common/params.cc
+++ common/params.cc
@@ -199,7 +199,6 @@ std::unordered_map<std::string, uint32_t> keys = {
     {"UpdaterTargetBranch", CLEAR_ON_MANAGER_START},
     {"UpdaterLastFetchTime", PERSISTENT},
     {"Version", PERSISTENT},
-    {"VisionRadarToggle", PERSISTENT},
 
     // Irene's stuff
     {"DisplayRadarInfo", PERSISTENT}, // display radar state on onroad UI
