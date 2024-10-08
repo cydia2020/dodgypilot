471376b3ab947eb3def086bb9c0f68574655c7f7
Irene <12470297+cydia2020@users.noreply.github.com>
2024-10-08 19:04:49 +1100

    Still off centre

--- selfdrive/ui/qt/onroad/model.cc
+++ selfdrive/ui/qt/onroad/model.cc
@@ -207,9 +207,9 @@ void ModelRenderer::drawLead(QPainter &painter, const cereal::RadarState::LeadDa
     // calculate the radar text box
     int radar_box_border = 6;
     int radar_box_padding_horizontal = 60; // horizontal padding
-    int radar_box_padding_vertical = 0; // vertical padding
+    int radar_box_padding_vertical = 10; // vertical padding
     int radar_box_offset = 80; // offset below the chevron
-    int radar_box_x = x - ((radar_text_width + radar_box_padding_horizontal + radar_box_border) / 2);
+    int radar_box_x = x - ((radar_text_width + radar_box_padding_horizontal) / 2);
     int radar_box_y = y + radar_box_offset;
     int radar_box_w = radar_text_width + radar_box_padding_horizontal;
     int radar_box_h = radar_text_height + radar_box_padding_vertical;
