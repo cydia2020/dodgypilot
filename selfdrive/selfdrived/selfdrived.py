192a7dcda6447d5653284be17d3c2a3cdf570594
Irene <12470297+cydia2020@users.noreply.github.com>
2025-01-09 15:04:17 +1100

    disable LDW and FCW

--- selfdrive/selfdrived/selfdrived.py
+++ selfdrive/selfdrived/selfdrived.py
@@ -85,7 +85,7 @@ class SelfdriveD:
 
     # read params
     self.is_metric = self.params.get_bool("IsMetric")
-    self.is_ldw_enabled = self.params.get_bool("IsLdwEnabled")
+    self.is_ldw_enabled = False
 
     car_recognized = self.CP.carName != 'mock'
 
@@ -336,11 +336,11 @@ class SelfdriveD:
         self.events.add(EventName.steerSaturated)
 
     # Check for FCW
-    stock_long_is_braking = self.enabled and not self.CP.openpilotLongitudinalControl and CS.aEgo < -1.25
-    model_fcw = self.sm['modelV2'].meta.hardBrakePredicted and not CS.brakePressed and not stock_long_is_braking
-    planner_fcw = self.sm['longitudinalPlan'].fcw and self.enabled
-    if (planner_fcw or model_fcw) and not self.CP.notCar:
-      self.events.add(EventName.fcw)
+#    stock_long_is_braking = self.enabled and not self.CP.openpilotLongitudinalControl and CS.aEgo < -1.25
+#    model_fcw = self.sm['modelV2'].meta.hardBrakePredicted and not CS.brakePressed and not stock_long_is_braking
+#    planner_fcw = self.sm['longitudinalPlan'].fcw and self.enabled
+#    if (planner_fcw or model_fcw) and not self.CP.notCar:
+#      self.events.add(EventName.fcw)
 
     # TODO: fix simulator
     if not SIMULATION or REPLAY:
